86c25756ed7fb83ca97861c8a1c3b62454a0c9c5
Pedrodiaxx <joel.diaz.lopez7@gmail.com>
2025-10-15 23:21:42 -0600

    feat(layout): add breadcrumb component and integrate into admin layout

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,11 @@
+<x-admin-layout :breadcrumbs="[
+    [
+        'name' => 'Dashboard',
+        'href' => route('admin.dashboard'),
+    ],
+    [
+        'name' => 'Profile',
+    ],
+]">
+    Hola, desde admin Pedriini
+</x-admin-layout>
